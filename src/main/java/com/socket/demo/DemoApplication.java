d56e7fdad399dbbb4e68d9e8ddd8097f39521c04
Shprins <1172336034@qq.com>
2019-09-03 13:08:10 +0800

    Merge b

--- src/main/java/com/socket/demo/DemoApplication.java
+++ src/main/java/com/socket/demo/DemoApplication.java
@@ -12,12 +12,12 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        Constant.setUrl("192.168.10.185:8005");
-        Dbconstant.setDbrecord(true);
+        Constant.setUrl("101.37.172.227:8005");
+        Dbconstant.setDbrecord(false);
         Dbconstant.setUsername("root");
         Dbconstant.setPassword("1234567");
         Dbconstant.setDriverClassName("com.mysql.jdbc.Driver");
-        Dbconstant.setUrl("jdbc:mysql://localhost:3306/practicee?characterEncoding=UTF8");
+        Dbconstant.setUrl("jdbc:mysql://localhost:3306/practicee?characterEncoding=UTF8&useUnicode=true&useSSL=false");
         Dbconstant.setUrlBean("com.socket.demo.po.");
         Convetutil.connectdata();
     }
